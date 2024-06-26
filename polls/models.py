06707ccb24e258df5eac5693e9354b0f209bd2ab
Leandro G~ <le.gonzalezm@duocuc.cl>
2024-06-25 23:21:18 -0400

    Se han agregado los archivos para empezar

---
+++ polls/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.IntegerField()
+    available = models.BooleanField(default=True)
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
